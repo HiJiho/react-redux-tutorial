dcaf522db20291780aa2084a082fb76ac7798732
김지호 <jiho001108@gmail.com>
2024-04-29 11:55:10 +0900

    fix index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import rootReducer from "./modules";
+import { Provider } from "react-redux";
 
 const store = createStore(rootReducer);
 
