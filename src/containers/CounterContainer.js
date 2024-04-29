bfc6869a0acd44b8913d19e92c5eed7fac2eefc3
김지호 <jiho001108@gmail.com>
2024-04-29 17:39:11 +0900

    불필요한 코드 블록 생략

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -6,18 +6,12 @@ const CounterContainer = ({ number, increase, decrease }) => {
 	return <Counter number={number} onIncrease={increase} onDecrese={decrease} />;
 };
 
-	// 스토어 상태를 컴포넌트의 props로 전달
-const mapStateToProps = (state) => ({
-	number: state.counter.number,
-});
-
-// 액션 생성 함수를 컴포넌트의 props로 전달
-const mapDispatchToProps = (dispatch) => ({
-	increase: () => {
-		dispatch(increase()); // increase(): 액션 생성 함수
-	},
-	decrease: () => {
-		dispatch(decrease()); // decrease(): 액션 생성 함수
-	},
-});
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default connect(
+	(state) => ({
+		number: state.counter.number,
+	}),
+	(dispatch) => ({
+		increase: () => dispatch(increase()),
+		decrease: () => dispatch(decrease()),
+	})
+)(CounterContainer);
