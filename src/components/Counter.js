1117d269b5c74f24890c04c8cacf50a43eb43511
김지호 <jiho001108@gmail.com>
2024-04-30 17:38:54 +0900

    useDispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,10 @@
-const Counter = ({ number, onIncrease, onDecrese }) => {
+const Counter = ({ number, onIncrease, onDecrease }) => {
 	return (
 		<div>
 			<h1>{number}</h1>
 			<div>
 				<button onClick={onIncrease}>+1</button>
-				<button onClick={onDecrese}>-1</button>
+				<button onClick={onDecrease}>-1</button>
 			</div>
 		</div>
 	);
--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,11 +1,22 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
+import { increase, decrease } from "../modules/counter";
 
 const CounterContainer = () => {
 	// 스토어 상태 선택
 	const number = useSelector((state) => state.counter.number);
+
+	// dispatch 정의
+	const dispatch = useDispatch();
+
 	// 스토어 상태를 props로 전달
-	return <Counter number={number} />;
+	return (
+		<Counter
+			number={number}
+			onIncrease={() => dispatch(increase())}
+			onDecrease={() => dispatch(decrease())}
+		/>
+	);
 };
 
 export default CounterContainer;
