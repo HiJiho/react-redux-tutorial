22936feee42255914479faa78be8cb9d023d4bda
김지호 <jiho001108@gmail.com>
2024-04-30 23:47:10 +0900

    createStore -> configureStore 변경

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import App from "./App";
 import { createStore } from "redux";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+// const store = createStore(
+//   rootReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+//   );
+const store = configureStore({
+	reducer: rootReducer,
+	devTools: process.env.NODE_ENV !== 'production', // 개발 환경에서만 DevTools 활성화
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
