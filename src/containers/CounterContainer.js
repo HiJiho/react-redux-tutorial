16975b859c2e0af8e53cdfd695eb60045ed2ccf0
김지호 <jiho001108@gmail.com>
2024-04-30 17:31:38 +0900

    useSelector

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,19 +1,11 @@
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import Counter from "../components/Counter";
-import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
-	return <Counter number={number} onIncrease={increase} onDecrese={decrease} />;
+const CounterContainer = () => {
+	// 스토어 상태 선택
+	const number = useSelector((state) => state.counter.number);
+	// 스토어 상태를 props로 전달
+	return <Counter number={number} />;
 };
 
-export default connect(
-	// 스토어 상태를 컴포넌트의 props로 전달
-	(state) => ({
-		number: state.counter.number,
-	}),
-	// 액션 생성 함수를 객체 형태로 넣으면 내부적으로 bindActionCreators 작업을 해줌
-	{
-		increase,
-		decrease,
-	}
-)(CounterContainer);
+export default CounterContainer;
