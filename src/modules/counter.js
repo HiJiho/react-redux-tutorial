d396407fe0f88163bba0ade08ecbc3e77761c079
김지호 <jiho001108@gmail.com>
2024-04-30 11:01:38 +0900

    redux-actions로 리듀서 함수 작성하기

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,31 +1,27 @@
+import { createAction, handleActions } from "redux-actions";
+
 // 액션 타입 정의
 // 문자열 안에 모듈 이름을 넣어 액션 이름 충돌 방지
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 // 액션 생성 함수 정의
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 모듈 초기 상태, 그리고 스토어의 초기 상태
 const initialState = {
 	number: 0,
 };
 
-// 리듀서 함수 -> state를 수정하는 방법 정의
-function counter(state = initialState, action) {
-	switch (action.type) {
-		case INCREASE:
-			return {
-				number: state.number + 1,
-			};
-		case DECREASE:
-			return {
-				number: state.number - 1,
-			};
-		default:
-			return state;
-	}
-}
+const counter = handleActions(
+	// 첫번째 인자: 각 액션에 대한 업데이트 함수
+	{
+		[INCREASE]: (state, action) => ({ number: state.number + 1 }),
+		[DECREASE]: (state, action) => ({ number: state.number - 1 }),
+	},
+	// 스토어의 초기 상태
+	initialState
+);
 
 export default counter;
