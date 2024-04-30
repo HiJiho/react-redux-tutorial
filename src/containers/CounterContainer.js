661e01becc5628ef565f16dad4d693a494acfc36
김지호 <jiho001108@gmail.com>
2024-04-30 17:47:52 +0900

    useCallback으로 useDispatch 최적화하기

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
@@ -8,14 +9,12 @@ const CounterContainer = () => {
 
 	// dispatch 정의
 	const dispatch = useDispatch();
+	const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+	const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
 
 	// 스토어 상태를 props로 전달
 	return (
-		<Counter
-			number={number}
-			onIncrease={() => dispatch(increase())}
-			onDecrease={() => dispatch(decrease())}
-		/>
+		<Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
 	);
 };
 
